2208db2b8dd2e742d0cda18e270e64eaee20a594
yasuabe2613 <yasu@yasuabe.net>
2022-08-11 01:47:17 +0900

    solve ex02

--- src/console/ex02.ts
+++ src/console/ex02.ts
@@ -2,14 +2,18 @@ import * as readline from 'readline';
 
 type callback = (s: string) => void;
 
+const nonEmpty = (data: string): boolean => typeof data === "string" && data.trim().length > 0;
+
 const input  = (f: (name: string, cb: callback) => void) =>
-  (cb: callback) => f('What is your name? ', cb);
-const concat = (n: string) => `Hello, ${n}, nice to meet you!`;
+  (cb: callback) => f('What is the input string? ', cb);
+const concat = (n: string) =>
+  nonEmpty(n) ? `${n} has ${n.length} characters!` : "Error: need non empty string";
+
 const output: callback = (s) => console.log(s);
 
 function main() {
   let rl = readline.createInterface({
-    input: process.stdin,
+    input:  process.stdin,
     output: process.stdout
   });
   input((s, cb) => rl.question(s, cb))((n) => {
